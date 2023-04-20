5dd01dc6aee0c8bd6cb614e8e003302616f3be14
vulook <andriy.dep@gmail.com>
2023-04-20 21:27:17 +0300

    Task03

---
+++ homework010_Git/Task03/geometry_lib/my_pyfiles/areas.py
@@ -0,0 +1,9 @@
+# This file contains functions to calculate areas of different shapes.
+
+def rectangle_area(length, width):
+    """Calculates the area of a rectangle with given length and width."""
+    return length * width
+
+def circle_area(radius):
+    """Calculates the area of a circle with given radius."""
+    return 3.14 * radius ** 2
---
+++ homework010_Git/Task03/geometry_lib/my_pyfiles/perimeters.py
@@ -0,0 +1,9 @@
+# This file contains functions to calculate perimeters of different shapes.
+
+def square_perimeter(side):
+    """Calculates the perimeter of a square with given side length."""
+    return 4 * side
+
+def triangle_perimeter(side1, side2, side3):
+    """Calculates the perimeter of a triangle with given side lengths."""
+    return side1 + side2 + side3
