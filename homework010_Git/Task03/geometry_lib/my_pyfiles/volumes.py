0d8af857d11536b990ff309da56cdaf1cd883c21
vulook <andriy.dep@gmail.com>
2023-04-20 21:31:26 +0300

    Task03

--- homework010_Git/Task03/geometry_lib/my_pyfiles/volumes.py
+++ homework010_Git/Task03/geometry_lib/my_pyfiles/volumes.py
@@ -1,9 +1,7 @@
 # This file contains functions to calculate volumes of different shapes.
 
 def cube_volume(side):
-    """Calculates the volume of a cube with given side length."""
     return side ** 3
 
 def sphere_volume(radius):
-    """Calculates the volume of a sphere with given radius."""
     return (4 / 3) * 3.14 * radius ** 3
